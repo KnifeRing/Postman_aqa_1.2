cb1cbd42ee31e84e6f93250fd3e8a07f46d43d34
Alexander Khyrbu <alexander.khyrbu@list.ru>
2024-05-13 20:58:18 +0300

    first commit

---
+++ src/test/java/ru/netology/PostmanTest.java
@@ -0,0 +1,9 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+}
