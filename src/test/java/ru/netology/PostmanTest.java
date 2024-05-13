1b50d8f1051f3421bb246d00025072f19357741b
Alexander Khyrbu <alexander.khyrbu@list.ru>
2024-05-13 21:04:39 +0300

    add PostmanTest

--- src/test/java/ru/netology/PostmanTest.java
+++ src/test/java/ru/netology/PostmanTest.java
@@ -1,9 +1,26 @@
 package ru.netology;
 
 import org.junit.jupiter.api.Test;
-
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static io.restassured.RestAssured.given;
 
 public class PostmanTest {
+    @Test
+    void shouldPostmanEchoTest() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Hello, world! test") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello, world! test"))
+        ;
+
+    }
 }
